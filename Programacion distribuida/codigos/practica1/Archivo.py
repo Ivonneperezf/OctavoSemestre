ba9e9dd54c54c5130aba7d71054d4e18a01be066
Ivonne <ivonne.perezf@alumno.buap.mx>
2025-01-18 00:25:38 -0600

    Cambios de archivos

---
+++ Programacion distribuida/codigos/practica1/Archivo.py
@@ -0,0 +1,5 @@
+class Archivo:
+    def __init__(self, nombre, ttl, publicacion):
+        self.nombre = nombre
+        self.ttl = ttl
+        self.publicacion = publicacion
--- Programacion distribuida/codigos/practica1/server.py
+++ Programacion distribuida/codigos/practica1/server.py
@@ -1,7 +1,5 @@
-#server.py
 import os
-import socket
-import json
+from Archivo import Archivo
 
 #Hilo que realice la conexion al socket
 #Hilo de actualizacion de archivos cada 5 minutos
@@ -11,50 +9,57 @@ import json
 class Server:
     def __init__(self):
         self.ruta = ""
-        self.contenido = []
-        self.archSelec = []
-        self.archComp = []
-        self.config = None
+        self.archivo = []
     
-    def listarArchivos(self): #Debe de insertar una ruta valida, modificar por si quiere listar la misma carpeta
+    def leerRuta(self): #Debe de insertar una ruta valida, modificar por si quiere listar la misma carpeta
         print("Inserte la ruta del directorio a conocer su listado")
         self.ruta = input()
-        self.contenido = os.listdir(self.ruta)
-    
-    def mostrarArchivos(self):
-        print("Los archivos disponibles son: ")
-        i = 1
-        for elemento in self.contenido:
-            print (i, ".- ", elemento)
-            i = i+1
-    
-    def seleccionarArchivos(self):
-        res = "S"
-        tiempo = 0
-        print("Agregue un archivo")
-        while(res == "S" or res == "s"):
-            print("Inserte que archivo desea agregar")
-            indice = int(input())
-            if (indice > len(self.contenido)+1 or indice < 1):
-                print("El indice de archivo no esta disponible")
-            else:
-                self.archSelec.append(indice)
-            print("Desea seguir agregando archivos? S/N")
-            res = input()
-        print("Inserte el tiempo de actualizacion TTL")
-        tiempo = int(input())
+        try:
+            contenido_bruto = os.listdir(self.ruta)
+            self.contenido = [archivo for archivo in contenido_bruto if '.' in archivo]
+            print("Archivos con extensión encontrados:")
+            print(self.contenido)
+        except FileNotFoundError:
+            print("La ruta proporcionada no es válida. Intente nuevamente.")
+        except PermissionError:
+            print("No tiene permiso para acceder a esta ruta.")
 
-        listado = {
-            "archivos" : self.archSelec,
-            "TTL" : tiempo
-        }
-        self.config = json.dumps(listado)
+    
+    
+    # def mostrarArchivos(self):
+    #     print("Los archivos disponibles son: ")
+    #     i = 1
+    #     for elemento in self.contenido:
+    #         print (i, ".- ", elemento)
+    #         i = i+1
+    
+    # def seleccionarArchivos(self):
+    #     res = "S"
+    #     tiempo = 0
+    #     print("Agregue un archivo")
+    #     while(res == "S" or res == "s"):
+    #         print("Inserte que archivo desea agregar")
+    #         indice = int(input())
+    #         if (indice > len(self.contenido)+1 or indice < 1):
+    #             print("El indice de archivo no esta disponible")
+    #         else:
+    #             self.archSelec.append(indice)
+    #         print("Desea seguir agregando archivos? S/N")
+    #         res = input()
+    #     print("Inserte el tiempo de actualizacion TTL")
+    #     tiempo = int(input())
+
+    #     listado = {
+    #         "archivos" : self.archSelec,
+    #         "TTL" : tiempo
+    #     }
+    #     self.config = json.dumps(listado)
 
 def main():
     server = Server()
-    server.listarArchivos()
-    server.mostrarArchivos()
-    server.seleccionarArchivos()
+    server.leerRuta()
+    # server.mostrarArchivos()
+    # server.seleccionarArchivos()
 
 if __name__ == "__main__":
     main()
